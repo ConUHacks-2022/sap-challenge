011da270c52a24b900410e37d03913b5b3d4e337
Shadi Jiha <shadosite@gmail.com>
2022-01-29 10:34:34 -0500

    Create theme.ts

---
+++ sap-challenge/theme.ts
@@ -0,0 +1,17 @@
+export default {
+	colors: {
+		transparent: "transparent",
+		black: "#000",
+		white: "#fff",
+		gray: {
+			50: "#f7fafc",
+			// ...
+			900: "#171923",
+		},
+		darkBlue: "#22223B",
+		bluish: "#4A4E69",
+		darkBeige: "#9A8C98",
+		beige: "#C9ADA7",
+		light: "#F2E9E4",
+	},
+};
